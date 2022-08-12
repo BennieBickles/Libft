b7ef2fa2895714b5c351faa2e72685734d070a44
BennieBickles <marie.tortrotau@gmail.com>
2022-08-12 15:01:23 +0200

    first one

---
+++ ft_isalnum.c
@@ -0,0 +1,6 @@
+int	ft_isalnum(int c)
+{
+	if (ft_isalpha(c) || ft_isdigit(c))
+		return (1);
+	return (0);
+}
---
+++ ft_isalpha.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+
+int	ft_is_alpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	return (0);
+}
