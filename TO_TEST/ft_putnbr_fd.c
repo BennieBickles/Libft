08622248ef2400ceac5ad5b3177887443f747ab2
BennieBickles <marie.tortrotau@gmail.com>
2022-09-02 17:01:16 +0200

    fd

---
+++ TO_TEST/ft_putnbr_fd.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+		write (fd, "-2147483648", 11);
+	else if (nbr < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr *= -1;
+	}
+	if (nbr > 9)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd(fd % 10 + '0', fd);
+}
