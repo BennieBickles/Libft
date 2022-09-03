0425f036e2d15a3ee9b2115910cb3d837d727ee0
BennieBickles <marie.tortrotau@gmail.com>
2022-09-03 17:33:28 +0200

    join sub

---
+++ TO_TEST/ft_strjoin.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (NULL);
+	while (*s1)
+		str[i++] = *s1++;
+	while (*s2)
+		str[i++] = *s2++;
+	str[i] = '\0';
+	return (str);
+}
---
+++ TO_TEST/ft_substr.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	slen;
+
+	slen = ft_strlen(s);
+	if (s == NULL)
+		return (NULL);
+	if (start > slen)
+		return (NULL);
+	if (start + len > slen)
+		len = slen - start;
+	if ((str = (char*)malloc(sizeof(char) * (len + 1))) == NULL)
+		return (NULL);
+	str[len] = '\0';
+	if (start > ft_strlen(s))
+		return (str);
+	return (ft_strncpy(str, s + start, len));
+}
