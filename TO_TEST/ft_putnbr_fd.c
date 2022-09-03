9fdf4108b04fd76bc542e7877aaf25676ffb4137
BennieBickles <marie.tortrotau@gmail.com>
2022-09-03 19:10:59 +0200

    putnbr

--- TO_TEST/ft_putnbr_fd.c
+++ TO_TEST/ft_putnbr_fd.c
@@ -4,12 +4,12 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
 		write (fd, "-2147483648", 11);
-	else if (nbr < 0)
+	else if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		nbr *= -1;
+		n *= -1;
 	}
-	if (nbr > 9)
-		ft_putnbr_fd(nbr / 10, fd);
+	if (n > 9)
+		ft_putnbr_fd(n / 10, fd);
 	ft_putchar_fd(fd % 10 + '0', fd);
 }
