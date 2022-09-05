d09f55094184e4fce5ce8671f32502641f0de13f
BennieBickles <marie.tortrotau@gmail.com>
2022-09-05 13:15:33 +0200

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -7,6 +7,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = -1;
 	while (++i < n && (*s1 || *s2))
 		if (s1[i] != s2[i])
-			return (((char*)s1)[i] - ((char*)s2)[i]);
+			return (((char *)s1)[i] - ((char *)s2)[i]);
 	return (0);
 }
