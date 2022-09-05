9deed359d2cf0d837dd3623bde85865f9728044a
BennieBickles <marie.tortrotau@gmail.com>
2022-09-05 11:43:41 +0200

    memmove opti

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,17 +2,12 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	int	i;
-
-	i = (int)len;
 	if (!dst || !src)
 		return (NULL);
 	if (dst > src)
-		while (i--)
-			*(char*)(dst + i) = *(char*)(src + i);
-	i = -1;
-	if (!(dst > src))
-		while (++i < (int)len)
-			*(char*)(dst + i) = *(char*)(src + i);
+		while (len--)
+			*(char*)(dst + len) = *(char*)(src + len);
+	else
+		ft_memcpy(dst, src, len);
 	return (dst);
 }
