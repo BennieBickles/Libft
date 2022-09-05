c1a37933701a3de38b2816ffda06f7d5fd82434e
BennieBickles <marie.tortrotau@gmail.com>
2022-09-05 11:51:59 +0200

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -8,10 +8,8 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	i = -1;
 	len = ft_strlen(src);
 	if (size)
-	{
 		while (++i < (size - 1) && len && src[i])
 			dest[i] = src[i];
-		dest[i] = '\0';
-	}
+	dest[i] = '\0';
 	return (len);
 }
