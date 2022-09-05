2d45ba0af6269d7ca9c1de605721d967794fecfa
BennieBickles <marie.tortrotau@gmail.com>
2022-09-05 11:27:26 +0200

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,23 +1,18 @@
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src,size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	*d;
-	unsigned char	*s;
+	int	i;
 
-	d = (unsigned char *)dst;
-	s = (unsigned char *)src;
-	if (src < dst)
-	{
-		s = s + len - 1;
-		d = d + len - 1;
-		while (len--)
-			*d-- = *s--;
-	}
-	else if (src >= dst)
-	{
-		while (len--)
-			*d++ = *s++;
-	}
+	i = (int)len;
+	if (!dst || !src)
+		return (NULL);
+	if (dst > src)
+		while (i--)
+			*(char*)(dst + i) = *(char*)(src + i);
+	i = -1;
+	if (!(dst > src))
+		while (++i < (int)len)
+			*(char*)(dst + i) = *(char*)(src + i);
 	return (dst);
 }
