dcfdf6d9331e52a553999a8bb27c3c254798e5f6
BennieBickles <marie.tortrotau@gmail.com>
2022-09-05 23:14:56 +0200

    Update

---
+++ ft_lstadd_back.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*end;
+
+	if (*lst)
+	{
+		end = ft_lstlast(*lst);
+		end->next = new;
+	}
+	else
+		*lst = new;
+}
---
+++ ft_lstclear.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	if ((*lst)->next)
+		ft_lstclear(&(*lst)->next, del);
+	ft_lstdelone(*lst, del);
+	*lst = NULL;
+}
