ea1022406b635dc5ca61b7334c440e71b3520b06
BennieBickles <marie.tortrotau@gmail.com>
2022-09-05 10:37:55 +0200

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -1,24 +1,13 @@
 #include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void	*b, int	c, size_t len)
 {
-	unsigned char	*pc;
-	unsigned long	*pl;
-	unsigned char	ch;
-	unsigned long	l;
-	size_t			nb;
+	char	*s;
 
-	pl = (unsigned long *)b;
-	ch = (unsigned char)c;
-	l = (unsigned long)ch;
-	nb = sizeof(unsigned long);
-	l *= 0x101010101010101;
-	nb = len / sizeof(unsigned long);
-	while (nb--)
-		*(pl++) = l;
-	nb = len % sizeof(unsigned long);
-	pc = (unsigned char *)pl;
-	while (nb--)
-		*(pc++) = ch;
+	s = (char *)b;
+	while (len--)
+	{
+		*s++ = (char)c;
+	}
 	return (b);
 }
