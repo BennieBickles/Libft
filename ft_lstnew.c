cb61f67f4ea478853c87ec3a1a8a041db88c82d3
BennieBickles <marie.tortrotau@gmail.com>
2022-09-06 19:24:31 +0200

    Update

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtortrot <mtortrot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/06 19:12:53 by mtortrot          #+#    #+#             */
+/*   Updated: 2022/09/06 19:12:55 by mtortrot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 t_list	*ft_lstnew(void *content)
 {
 	t_list	*new;
 
-	new = (t_list*)malloc(sizeof(t_list));
+	new = (t_list *)malloc(sizeof(t_list));
 	if (!new)
 		return (NULL);
 	new->content = content;
