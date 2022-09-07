2b55ab37b954d6c4232001dca5243de6169a9efe
BennieBickles <marie.tortrotau@gmail.com>
2022-09-07 09:23:18 +0200

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mtortrot <mtortrot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 19:07:57 by mtortrot          #+#    #+#             */
-/*   Updated: 2022/09/06 19:07:59 by mtortrot         ###   ########.fr       */
+/*   Updated: 2022/09/07 09:22:22 by mtortrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*str;
 
 	str = malloc(nmemb * size);
-	if (!(str))
+	if (!(str) || (nmemb * size) > PTRDIFF_MAX
+		|| nmemb > PTRDIFF_MAX || size > PTRDIFF_MAX)
 		return (NULL);
 	ft_bzero(str, (nmemb * size));
 	return (str);
